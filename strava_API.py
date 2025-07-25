7b7b74e944b3b6ddec306675c49a0d138d68d63c
acstein <astein98@gmail.com>
2025-07-25 11:10:49 +0100

    Addition of dash app

--- strava_API.py
+++ strava_API.py
@@ -8,6 +8,10 @@ from pandas import json_normalize
 
 import login as login
 
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+
 """
 API Set-up and Handling
 """
@@ -52,9 +56,68 @@ for activity in data:
         run_data.append(activity)
 
 run_data = json_normalize(run_data)
+new_df = pd.DataFrame()
 
-run_data['Distance / km'] = run_data['distance'].apply(lambda x: x/1000)
-run_data['Time / minutes'] = run_data['elapsed_time'].apply(lambda x: x/60)
+new_df['Distance / km'] = run_data['distance'].apply(lambda x: x/1000)
+new_df['Time / minutes'] = run_data['elapsed_time'].apply(lambda x: x/60)
+new_df['Avg Heartrate'] = run_data['average_heartrate'].apply(lambda x: x if not pd.isna(x) else 10)
+new_df['Elevation / m'] = run_data['total_elevation_gain'].apply(lambda x: x if not pd.isna(x) else 10)
+new_df['Average Speed / ms-1'] = run_data['average_speed']
+new_df['Max Speed / ms-1'] = run_data['max_speed']
+new_df['Start Date'] = run_data['start_date']
 
-plt.scatter(run_data['Distance / km'], run_data['Time / minutes'])
-plt.show()
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4('Strava Running App'),
+
+    html.P("Select Metric:"),
+    dcc.Dropdown(
+        id="dropdown",
+        options=[
+            {'label': 'Elevation / m', 'value': 'Elevation / m'},
+            {'label': 'Average Speed / ms-1', 'value': 'Average Speed / ms-1'},
+            {'label': 'Max Speed / ms-1', 'value': 'Max Speed / ms-1'},
+        ],
+        value='Elevation / m',
+        clearable=False,
+    ),
+
+    html.P("Filter by Start Date:"),
+    dcc.DatePickerRange(
+        id='date-picker',
+        start_date=new_df['Start Date'].min(),
+        end_date=new_df['Start Date'].max(),
+        display_format='YYYY-MM-DD'
+    ),
+
+    dcc.Graph(id="graph"),
+])
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("dropdown", "value"),
+    Input("date-picker", "start_date"),
+    Input("date-picker", "end_date")
+)
+def update_graph(selected_value, start_date, end_date):
+    # Filter by date range
+    mask = (new_df['Start Date'] >= start_date) & (new_df['Start Date'] <= end_date)
+    filtered_df = new_df.loc[mask]
+
+    # Create figure
+    fig = px.scatter(
+        filtered_df,
+        x='Distance / km',
+        y='Time / minutes',
+        size='Avg Heartrate',
+        color=selected_value
+    )
+    return fig
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# ToDo: unit tests and type stuff
